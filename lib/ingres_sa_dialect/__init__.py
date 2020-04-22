746b47822d2050d5dce13c518fd25d870e7f283a
Chris Clark <Chris.Clark@actian.com>
2020-04-22 10:43:50 -0700

    Temp remove zxJDBC support -  SQLAlchemy 1.4.0b1 does not appear to include support

--- lib/ingres_sa_dialect/__init__.py
+++ lib/ingres_sa_dialect/__init__.py
@@ -3,6 +3,6 @@
 #
 # This module is part of SQLAlchemy and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
-from ingres_sa_dialect import base, ingresdbi, pyodbc, zxjdbc
+from ingres_sa_dialect import base, ingresdbi, pyodbc  # , zxjdbc  # does not appear to be in SQLAlchemy 1.4.0b1
 
 base.dialect = pyodbc.Ingres_pyodbc
