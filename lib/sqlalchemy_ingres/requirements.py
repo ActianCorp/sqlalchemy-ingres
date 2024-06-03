7f915b4d43a801fd3aef06a78fec648e92c22582
Michael Habiger <115743596+hab6@users.noreply.github.com>
2024-06-03 14:54:24 -0500

    Mitigate AssertionError for unique constraints (#56)
    * Mitigate AssertionError for unique constraints

--- lib/sqlalchemy_ingres/requirements.py
+++ lib/sqlalchemy_ingres/requirements.py
@@ -2,18 +2,21 @@ from sqlalchemy.testing.requirements import SuiteRequirements
 
 from sqlalchemy.testing import exclusions
 
+supported = exclusions.open
+unsupported = exclusions.closed
+
 class Requirements(SuiteRequirements):
     @property
     def date_historic(self):
         """target dialect supports representation of Python
         datetime.datetime() objects with historic (pre 1970) values."""
-        return exclusions.open()
+        return supported()
 
     @property
     def datetime_historic(self):
         """target dialect supports representation of Python
         datetime.datetime() objects with historic (pre 1970) values."""
-        return exclusions.open()
+        return supported()
 
     @property
     def datetime_literals(self):
@@ -21,13 +24,13 @@ class Requirements(SuiteRequirements):
         literal string, e.g. via the TypeEngine.literal_processor() method.
 
         """
-        return exclusions.open()
+        return supported()
 
     @property
     def identity_columns(self):
         """If a backend supports GENERATED { ALWAYS | BY DEFAULT }
         AS IDENTITY"""
-        return exclusions.open()
+        return supported()
 
     @property
     def identity_columns_standard(self):
@@ -35,5 +38,18 @@ class Requirements(SuiteRequirements):
         AS IDENTITY with a standard syntax.
         This is mainly to exclude MSSql.
         """
-        return exclusions.open()
+        return supported()
+
+    @property
+    def array_type(self):
+        return supported()
+
+    @property
+    def unique_constraint_reflection(self):
+        return supported()
+
+    @property
+    def temp_table_reflection(self):
+        return unsupported()
+
 
